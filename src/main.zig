1330ca7a766e5dd6cc73be083751e066fa881192
Nuwan Rajika Kumarasiri <nrajikak@gmail.com>
2026-04-19 15:47:32 -0500

    add notes

---
+++ src/main.zig
@@ -0,0 +1,87 @@
+const std = @import("std");
+const partInt = std.fmt.parseInt;
+
+const tokenizer = @import("tokenizer.zig");
+
+pub fn main() !void {
+    var debugAllocator = std.heap.DebugAllocator(.{
+        .stack_trace_frames = 16,
+    }).init;
+    defer _ = debugAllocator.deinit();
+
+    const gpa = debugAllocator.allocator();
+    const args = try parseArgs();
+    try run(gpa, args);
+}
+
+pub fn run(allocator: std.mem.Allocator, args: Args) !void {
+    const path = args.path;
+
+    // compiler
+    {
+        const src = try std.fs.cwd().readFileAllocOptions(
+            allocator,
+            path,
+            std.math.maxInt(usize),
+            null,
+            std.mem.Alignment.fromByteUnits(8),
+            0,
+        );
+        defer allocator.free(src);
+
+        var tok = tokenizer.Tokenizer.init(src);
+
+        if (args.mode == .tokenizer) {
+            while (tok.next()) |token| {
+                std.debug.print("{}: {s}\n", .{
+                    token.tag,
+                    src[token.loc.start..token.loc.end],
+                });
+
+                switch (token.tag) {
+                    .invalid => return error.LexerError,
+                    .eof => break,
+                    else => {},
+                }
+            }
+
+            return;
+        }
+    }
+}
+
+const Args = struct {
+    path: []const u8,
+    mode: Mode,
+};
+
+const Mode = enum {
+    tokenizer,
+    parse,
+    compile,
+    codegen,
+};
+
+fn parseArgs() !Args {
+    // zig build run -- lex assets/intro.typ
+    var args = std.process.args();
+    _ = args.skip();
+
+    var path: []const u8 = "";
+    var mode: Mode = .compile;
+
+    var index: u8 = 0;
+    while (args.next()) |arg| {
+        if (index > 1) {
+            return error.InvalidArgument;
+        } else if (index == 0) {
+            mode = std.meta.stringToEnum(Mode, arg[0..]) orelse
+                return error.UnrecognizedFlag;
+        } else {
+            path = arg;
+        }
+        index += 1;
+    }
+
+    return .{ .path = path, .mode = mode };
+}
---
+++ src/tokenizer.zig
@@ -0,0 +1,245 @@
+const std = @import("std");
+
+const mem = std.mem;
+const testing = std.testing;
+const expect = testing.expect;
+const pageAllocator = std.heap.page_allocator;
+
+pub const Token = struct {
+    tag: Tag,
+    loc: Loc,
+
+    pub const Loc = struct {
+        start: usize,
+        end: usize,
+    };
+
+    pub const Tag = enum {
+        invalid,
+        identifier,
+        eof,
+        err,
+        shebang,
+        line_comment,
+        block_comment,
+        markup,
+        text,
+        space,
+        linebreak,
+        paragrah_break,
+        escape,
+        shorthand,
+        smartquote,
+        strong,
+        empasized,
+        raw,
+        raw_lang,
+        raw_delim,
+        raw_trimmed,
+        link,
+        label,
+        ref,
+        ref_marker,
+        heading,
+        heading_marker,
+        list_item,
+        list_marker,
+        enum_item,
+        enum_marker,
+        term_item,
+        term_marker,
+        eq,
+
+        math,
+        math_text,
+        math_ident,
+        math_shorthand,
+        math_alignpoint,
+        math_call,
+        math_args,
+        math_delim,
+        math_attach,
+        math_primes,
+        math_frac,
+        math_root,
+
+        hash,
+        l_brace,
+        r_brace,
+        l_bracket,
+        r_bracket,
+        l_paren,
+        r_paren,
+        comma,
+        colon,
+        semicolon,
+        star,
+        underscore,
+        dollar,
+        plus,
+        minus,
+        slash,
+        caret,
+        dot,
+        equal,
+        equal_equal,
+        bang_equal,
+        angle_bracket_left,
+        angle_bracket_left_equal,
+        angle_bracket_right,
+        angle_bracket_right_equal,
+        plus_equal,
+        minus_euqal,
+        asterisk_equal,
+        slash_equal,
+        dot_dot,
+        equal_angle_bracket_left,
+        root,
+        bang,
+
+        op_not,
+        op_and,
+        op_or,
+
+        none,
+        auto,
+
+        keyword_let,
+        keyword_set,
+        keyword_show,
+        keyword_context,
+        keyword_if,
+        keyword_else,
+        keyword_for,
+        keyword_in,
+        keyword_while,
+        keyword_break,
+        keyword_continue,
+        keyword_return,
+        keyword_import,
+        keyword_include,
+        keyword_as,
+
+        code,
+        boolean,
+        int,
+        float,
+        numeric,
+        str,
+        code_block,
+        content_block,
+        parenthesized,
+        array,
+        dict,
+        named,
+        keyed,
+        unary,
+        binary,
+        field_access,
+        func_call,
+        args,
+        spread,
+        closure,
+        params,
+        let_binding,
+
+        rule_set,
+        rule_show,
+
+        contextual,
+        conditional,
+        while_loop,
+        for_loop,
+        import_module,
+        import_items,
+        import_item_path,
+        rename_import_item,
+        module_include,
+        destructuring,
+        destruct_assignment,
+    };
+
+    pub fn lexme(tag: Tag) ?[]const u8 {
+        return switch (tag) {
+            .invalid,
+            .identifier,
+            .eof,
+            => null,
+
+            .bang => "!",
+            .equal => "=",
+            .equal_equal => "==",
+            .equal_angle_bracket_right => "=>",
+            .bang_equal => "!=",
+            .l_paren => "(",
+            .r_paren => ")",
+            .l_brace => "{",
+            .r_brace => "}",
+            .l_bracket => "[",
+            .r_bracket => "]",
+            .period => ".",
+            .caret => "^",
+            .plus => "+",
+            .plus_equal => "+=",
+        };
+    }
+};
+
+pub const Tokenizer = struct {
+    buffer: [:0]const u8,
+    index: usize,
+
+    pub fn dump(self: *Tokenizer, token: *const Token) void {
+        std.debug.print("{s} \"{s}\"\n", .{ @tagName(token.tag), self.buffer[token.loc.start..token.loc.end] });
+    }
+
+    pub fn init(buffer: [:0]const u8) Tokenizer {
+        // skip the UTF-8 BOF if present.
+        return .{
+            .buffer = buffer,
+            .index = if (std.mem.startsWith(u8, buffer, "\xEF\xBB\xBF")) 3 else 0,
+        };
+    }
+
+    pub const State = enum {
+        start,
+        invalid,
+    };
+
+    pub fn next(self: *Tokenizer) ?Token {
+        var result: Token = .{
+            .tag = undefined,
+            .loc = .{
+                .start = self.index,
+                .end = undefined,
+            },
+        };
+        state: switch (State.start) {
+            .start => switch (self.buffer[self.index]) {
+                0 => {
+                    if (self.index == self.buffer.len) {
+                        return .{
+                            .tag = .eof,
+                            .loc = .{
+                                .start = self.index,
+                                .end = self.index,
+                            },
+                        };
+                    } else {
+                        continue :state .invalid;
+                    }
+                },
+                ' ', '\n', '\t', '\r' => {
+                    self.index += 1;
+                    result.loc.start = self.index;
+                    continue :state .start;
+                },
+            },
+            .invalid => {
+                std.debug.print();
+            },
+        }
+        result.loc.end = self.index;
+        return result;
+    }
+};
